76177725ff520085e7a4bd687221f9e01da6ee62
Raghu Teja <raghutejaparisa@gmail.com>
2014-11-30 23:31:31 +0530

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PersonGap::Application.config.secret_token = '4b33a42cf103dae6a6162b78329210650915485825dcb7320a182e3554ad1b521ab436c37fcd96b5ec138920f8803094e8ae5ccdd5ce7cf0c6d916a626e790fe'
